1cee742fe8068d593cafc2accbca8f24ec7dac94
Галина <g416@mail.ru>
2023-04-26 21:01:26 +0500

    классы и объекты/конструкторы и геттеры готовы

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private Author authorBook;
+    private String nameBook;
+    private int publicationDateBook;
+
+    public Book( String nameBook, int publicationDateBook, Author authorBook) {
+        this.authorBook = authorBook;
+        this.nameBook = nameBook;
+        this.publicationDateBook = publicationDateBook;
+
+    }
+    public Author getAuthorBook (){
+        return this.authorBook;
+    }
+    public String getNameBook() {
+        return this.nameBook;
+    }
+    public int getPublicationDateBooK() {
+        return this.publicationDateBook;
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    Author AuthorBookBookThreeMusketeers = new Author("Александр","Дюма");
+   Author authorBookThickAngThin = new Author("Антон","Чехов");
+   Book threeMusketeers = new Book("Три мушкетера", 1844, AuthorBookBookThreeMusketeers);
+   Book thickAngThin = new Book("Толстый и тонкий", 1844 , authorBookThickAngThin);
+        System.out.println("threeMusketeers.getAuthorBook().getFirstName() = " + threeMusketeers.getAuthorBook().getFirstName());
+        System.out.println("threeMusketeers.getAuthorBook().getLastName() = " + threeMusketeers.getAuthorBook().getLastName());
+        System.out.println("threeMusketeers.getNameBook() = " + threeMusketeers.getNameBook());
+        System.out.println("threeMusketeers.getPublicationDateBooK() = " + threeMusketeers.getPublicationDateBooK());
+        System.out.println("thickAngThin.getAuthorBook().getFirstName() = " + thickAngThin.getAuthorBook().getFirstName());
+        System.out.println("thickAngThin.getAuthorBook().getLastName() = " + thickAngThin.getAuthorBook().getLastName());
+        System.out.println("thickAngThin.getNameBook() = " + thickAngThin.getNameBook());
+        System.out.println("thickAngThin.getPublicationDateBooK() = " + thickAngThin.getPublicationDateBooK());
+
     }
+
 }
